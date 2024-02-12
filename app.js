3df1f18c0fd99bc8d493dc8e291f805557ee1df3
Fahad Khan <138850947+fk78691@users.noreply.github.com>
2024-02-12 14:22:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Javascript GSAP//
+gsap.from("#text1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
+gsap.from("#firstcontent",{
+    delay:0.4,
+    opacity:0,
+    duration:3,
+})
+gsap.from("#navbar",{
+    height:-800,
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
+// Javascript GSAP//
